6d120e87dac241319b6bdeb241b0c3fa2daf4353
pateldaksh55 <patel.daksh.7792@gmail.com>
2025-02-11 18:31:30 +0530

    env

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ app.use(cors());
 app.use(express.static("public")); // Serve static files
 
 // Connect to MongoDB
-mongoose.connect("mongodb+srv://pateldaksh7792:<db_password>@cluster0.ph4xj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
